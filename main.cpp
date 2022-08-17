46eb723d87a9940331f8165f605c7c1c5cfedfde
XuWilliam95 <107287560+XuWilliam95@users.noreply.github.com>
2022-08-17 01:34:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,32 @@
 #include <list>
 #include <string>
 #include <time.h>
+
+
 using namespace std;
 
-
-
 int main() {
+
+int minutes = 0;
+int milliseconds = 0;
+
+cout << "how long do you want to go for.";
+cin >> minutes;
+
+clock_t timer;
+timer  = clock();
+
+milliseconds = minutes * 60000;
 
 string strike_type[6] = {"jab", "cross", "lead hook", "rear hook", "lead uppercut", "rear uppercut"};
 string strike_loc[2] = {"head", "body"}; 
 
-srand( time(NULL));
 
+while (milliseconds = clock()){
+srand( time(NULL));
 int pick_strike = rand() % 6;
 int pick_loc = rand() % 2;
-
-
-
 cout << strike_type[pick_strike] <<" "<< strike_loc[pick_loc]<< "\n";
+}
 
 }
